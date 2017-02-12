3f88e5169bc031aae730c857605003b2ce4b7a88
ValentinZhao <2264709714@qq.com>
2017-02-13 00:12:40 +0800

    third party widgets import

--- app/src/main/java/com/jyutwaa/zhaoziliang/glimpse/Activity/ZhihuDetailPageActivity.java
+++ app/src/main/java/com/jyutwaa/zhaoziliang/glimpse/Activity/ZhihuDetailPageActivity.java
@@ -2,10 +2,6 @@ package com.jyutwaa.zhaoziliang.glimpse.Activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.jyutwaa.zhaoziliang.glimpse.Model.Zhihu.ZhihuDetailPage;
 import com.jyutwaa.zhaoziliang.glimpse.Presenter.viewImpl.IZhihuDetailPage;
@@ -27,29 +23,6 @@ public class ZhihuDetailPageActivity extends BaseDetailPageActivity implements I
         super.onCreate(savedInstanceState);
         setContentView(R.layout.zhihu_detail_layout);
         getWindow().setStatusBarColor(getResources().getColor(R.color.immersive_bars));
-        ListView mListView = (ListView) findViewById(R.id.listView);
-        for (int i = 0; i <= 30; i++) {
-            list.add("测试数据" + i);
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                ZhihuDetailPageActivity.this, android.R.layout.simple_list_item_1, list){
-
-            @Override
-            public int getCount() {
-                return super.getCount();
-            }
-
-            @Override
-            public View getView(int position, View convertView,
-                                ViewGroup parent) {
-
-                System.out.println("getView");
-                return super.getView(position, convertView, parent);
-            }
-
-        };
-        mListView.setAdapter(adapter);
-
 
 
     }
