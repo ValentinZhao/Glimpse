929c511278cdd5d47619178c1b50c623060ec166
ValentinZhao <2264709714@qq.com>
2017-03-01 22:31:34 +0800

    cover fragment accomplish

--- app/src/main/java/com/jyutwaa/zhaoziliang/glimpse/Adapter/Bilibili/CoverAdapter.java
+++ app/src/main/java/com/jyutwaa/zhaoziliang/glimpse/Adapter/Bilibili/CoverAdapter.java
@@ -1,16 +1,30 @@
 package com.jyutwaa.zhaoziliang.glimpse.Adapter.Bilibili;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.ColorMatrixColorFilter;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.jyutwaa.zhaoziliang.glimpse.Model.Bilibili.TopListTypeItem;
 import com.jyutwaa.zhaoziliang.glimpse.R;
 import com.jyutwaa.zhaoziliang.glimpse.Utils.ViewUtils;
 import com.jyutwaa.zhaoziliang.glimpse.Widgets.BadgedFourThreeImageView;
+import com.jyutwaa.zhaoziliang.glimpse.Widgets.DribbbleTarget;
+import com.jyutwaa.zhaoziliang.glimpse.Widgets.ObservableColorMatrix;
 
 import java.util.ArrayList;
 
@@ -24,11 +38,12 @@ public class CoverAdapter extends RecyclerView.Adapter {
     private static final int TYPE_LOADING = -1;
 
     private Context mContext;
-    private ArrayList<TopListTypeItem> mTopListTypeItems;
+    private ArrayList<TopListTypeItem> mTopListTypeItems = new ArrayList<>();
 
     private int widthPx;
     private int heightPx;
     private float width;
+    private String videoUrl;
 
     public CoverAdapter(Context mContext) {
         this.mContext = mContext;
@@ -80,11 +95,80 @@ public class CoverAdapter extends RecyclerView.Adapter {
     }
 
     private void bindLoadingViewHolder(LoadingViewHolder holder, int position) {
-
+        holder.progressBar.setVisibility(View.VISIBLE);
     }
 
-    private void bindNormalViewHolder(NormalViewHolder holder, int position) {
+    private void bindNormalViewHolder(final NormalViewHolder holder, int position) {
+        final TopListTypeItem item = mTopListTypeItems.get(holder.getAdapterPosition());
+        videoUrl = item.getVideoUrl();
+        //        if(DBUtils.getDB(mContext).isRead(Config.BILIBILI, item.getAid(), 1)){
+//            holder.itemTitle.setTextColor(Color.GRAY);
+//        } else {
+//            holder.itemTitle.setTextColor(Color.BLACK);
+//        }
+        holder.itemImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "进入视频详情页", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.itemTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "进入视频详情页", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.itemComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "进入评论区", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.itemTitle.setText(item.getTitle());
+        holder.itemDescription.setText(item.getDescription());
+        holder.itemAuthor.setText(item.getAuthor());
+        holder.itemFavorite.setText(item.getFavoritesCount());
+        holder.itemComment.setText(item.getCommentCount());
+        Glide.with(mContext)
+                .load(mTopListTypeItems.get(position).getIconUrl())
+                .listener(new RequestListener<String, GlideDrawable>() {
+                    @Override
+                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                        return false;
+                    }
 
+                    @Override
+                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                        if (!item.hasFadedIn) {
+                            holder.itemImage.setHasTransientState(true);
+                            final ObservableColorMatrix cm = new ObservableColorMatrix();
+                            final ObjectAnimator animator = ObjectAnimator.ofFloat(cm, ObservableColorMatrix.SATURATION, 0f, 1f);
+                            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                                @Override
+                                public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                                    holder.itemImage.setColorFilter(new ColorMatrixColorFilter(cm));
+                                }
+                            });
+                            animator.setDuration(2000L);
+                            animator.setInterpolator(new AccelerateInterpolator());
+                            animator.addListener(new AnimatorListenerAdapter() {
+                                @Override
+                                public void onAnimationEnd(Animator animation) {
+                                    super.onAnimationEnd(animation);
+                                    holder.itemImage.clearColorFilter();
+                                    holder.itemImage.setHasTransientState(false);
+                                    animator.start();
+                                    item.hasFadedIn = true;
+
+                                }
+                            });
+                        }
+
+                        return false;
+                    }
+                }).diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .centerCrop().override(widthPx, heightPx)
+                .into(new DribbbleTarget(holder.itemImage, false));
     }
 
     public void addItems(ArrayList<TopListTypeItem> items){
